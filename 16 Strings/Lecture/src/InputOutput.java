9aaaabea75ded37c48898ea71844c3dc626a7992
Gaurav999-byte <nagralegaurav16@gmail.com>
2025-06-25 14:53:00 +0530

    add

--- 16 Strings/Lecture/src/InputOutput.java
+++ 16 Strings/Lecture/src/InputOutput.java
@@ -1,2 +1,13 @@
+import java.util.*;
+
 public class InputOutput {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+
+        /* String name = sc.next() //For Single Word */
+
+        System.out.print("Entre your name:");
+        String name = sc.nextLine();
+        System.out.println(name+" is Student of Ghrce");
+    }
 }
